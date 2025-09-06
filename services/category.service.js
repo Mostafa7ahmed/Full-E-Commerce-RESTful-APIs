afa6be6df8265ee9b760c54b08cfa6d4cb025622
mostafa hamed <Mostafa7ahmed>
2025-09-06 05:56:10 +0300

    using asyncHandler

--- services/category.service.js
+++ services/category.service.js
@@ -1,4 +1,6 @@
 const categoryModel = require("../Models/category.module");
+const asyncHandler = require('express-async-handler')
+
 const getCategories = async (req, res) => {
   try {
     const categories = await categoryModel.find();
@@ -11,33 +13,18 @@ const getCategories = async (req, res) => {
     res.status(500).json({ error: "Error fetching categories", details: err });
   }
 };
-const createCategory = async (req, res) => {
-  categoryModel
-    .create({
-      name,
-      slug: name.replace(/\s+/g, "-").toLowerCase(),
-    })
-    .then((category) => {
-      res.status(201).json({
+const createCategory = asyncHandler(async (req, res) => {
+  const { name } = req.body;
+  const category = await categoryModel.create({name, slug: name.replace(/\s+/g, "-").toLowerCase(),});
+    res.status(201).json({
         status: "success",
         message: "Category created successfully",
         data: category,
       });
-    })
-    .catch((err) => {
-      if (err.code === 11000) {
-        return res.status(400).json({
-          status: "fail",
-          message: "Category name must be unique",
-        });
-      }
-      res.status(400).json({
-        status: "fail",
-        message: "Category not created",
-        error: err.message,
-      });
-    });
-};
+
+   
+    
+});
 
 module.exports = {
   getCategories,
